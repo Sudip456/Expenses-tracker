b4cddae215f8d621da170a6b9b03b7b0b2656802
Sudip Dhakal <dhakalsudip246@gmail.com>
2024-11-08 11:45:26 +0545

    App.jsx
    new file added

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"
+import Expense_main from "./Expense tracker/Expense_main.js"
+ 
+
+function App(){
+
+  return(
+    < Expense_main />
+  )
+}
+
+ 
+
+export default App;
